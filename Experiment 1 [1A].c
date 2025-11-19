feb1427d707631060e5644704c6f33db0aae7812
ismail8994-stack <ismail8994@eng.rizvi.edu.in>
2025-11-19 21:29:48 +0530

    Create Experiment 1 [1A].c

---
+++ Experiment 1 [1A].c
@@ -0,0 +1,17 @@
+/*
+Name: ISMAIL PATEL
+UIN: 251M042
+Class: FE Mechanical
+Div: F
+*/
+#include <stdio.h>
+
+int main() {
+    printf("Your name is Ashaz Shakir Patel\n");
+    printf("Age: 18 yrs\n");
+    printf("Class: FE Mechanical\n");
+    printf("Division: F\n");
+    printf("UIN: 251M003\n");
+
+    return 0;
+}
