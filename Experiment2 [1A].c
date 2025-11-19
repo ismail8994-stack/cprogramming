fbecfd48c7ceb976a7cf07ad2a10757748faaefe
ismail8994-stack <ismail8994@eng.rizvi.edu.in>
2025-11-19 21:41:25 +0530

    Create Experiment2 [1A].c

---
+++ Experiment2 [1A].c
@@ -0,0 +1,26 @@
+/*
+Name:ISMAIL PATEL
+UIN:251M042
+Class:FE Mechanical
+Div: F
+*/
+#include <stdio.h>
+
+int main() 
+{
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0) 
+    {
+    printf("The number %d is even.\n", number);
+    } 
+    else 
+    {
+    printf("The number %d is odd.\n", number);
+    }
+
+    return 0;
+}
